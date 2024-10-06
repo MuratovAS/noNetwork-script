2f8d7856299bc36aa673cf6f262381dbd07ec20a
Aleksej Muratov <50487552+MuratovAS@users.noreply.github.com>
2024-10-06 15:04:43 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 PATH_RULE="/etc/NetworkManager/dispatcher.d/pre-up.d"
 PATH_EXEC="/usr/local/bin"
 
@@ -11,30 +11,42 @@ sudo gpasswd -a $USER noinet
 sudo gpasswd -a $USER nolan
 
 #RULE
-echo "\
-#!/bin/bash
-iptables -A OUTPUT -d 10.0.0.0/8 -m owner --gid-owner noinet -j ACCEPT
-iptables -A OUTPUT -d 172.16.0.0/12 -m owner --gid-owner noinet -j ACCEPT
-iptables -A OUTPUT -d 192.168.0.0/16 -m owner --gid-owner noinet -j ACCEPT
-iptables -A OUTPUT -m owner --gid-owner noinet -j DROP
-" | sudo tee $PATH_RULE/noinet_rule
+echo '\
+#!/bin/sh
+if [ $1 = "lo" ]; then
+  # rfc3330
+  iptables -A OUTPUT -d 240.0.0.0/4 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -d 224.0.0.0/4 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -d 198.18.0.0/15 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -d 192.168.0.0/16 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -d 192.88.99.0/24 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -d 192.0.2.0/24 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -d 172.16.0.0/12 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -d 169.254.0.0/16 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -d 127.0.0.0/8 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -d 10.0.0.0/8 -m owner --gid-owner noinet -j ACCEPT
+  iptables -A OUTPUT -m owner --gid-owner noinet -j DROP
+fi
+' | sudo tee $PATH_RULE/noinet_rule
 sudo chmod +x $PATH_RULE/noinet_rule
 
-echo "\
-#!/bin/bash
-iptables -A OUTPUT -m owner --gid-owner nolan -j DROP
-" | sudo tee $PATH_RULE/nolan_rule
+echo '\
+#!/bin/sh
+if [ $1 = "lo" ]; then
+  iptables -A OUTPUT -m owner --gid-owner nolan -j DROP
+fi
+' | sudo tee $PATH_RULE/nolan_rule
 sudo chmod +x $PATH_RULE/nolan_rule
 
 #EXEC
 echo '
-#!/bin/bash
+#!/bin/sh
 sg noinet "$*"
 ' | sudo tee $PATH_EXEC/noinet
 sudo chmod +x $PATH_EXEC/noinet
 
 echo '
-#!/bin/bash
+#!/bin/sh
 sg nolan "$*"
 ' | sudo tee $PATH_EXEC/nolan
 sudo chmod +x $PATH_EXEC/nolan
