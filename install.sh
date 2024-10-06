9861704fdc4011d15c722b4d1171675d5e1abe22
Aleksej Muratov <50487552+MuratovAS@users.noreply.github.com>
2024-10-06 15:14:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 PATH_RULE="/etc/NetworkManager/dispatcher.d/pre-up.d"
 PATH_EXEC="/usr/local/bin"
 
@@ -11,9 +11,8 @@ sudo gpasswd -a $USER noinet
 sudo gpasswd -a $USER nolan
 
 #RULE
-echo '\
-#!/bin/sh
-if [ $1 = "lo" ]; then
+echo '#!/bin/sh
+if [ "$1" = "lo" ]; then
   # rfc3330
   iptables -A OUTPUT -d 240.0.0.0/4 -m owner --gid-owner noinet -j ACCEPT
   iptables -A OUTPUT -d 224.0.0.0/4 -m owner --gid-owner noinet -j ACCEPT
@@ -26,36 +25,29 @@ if [ $1 = "lo" ]; then
   iptables -A OUTPUT -d 127.0.0.0/8 -m owner --gid-owner noinet -j ACCEPT
   iptables -A OUTPUT -d 10.0.0.0/8 -m owner --gid-owner noinet -j ACCEPT
   iptables -A OUTPUT -m owner --gid-owner noinet -j DROP
-fi
-' | sudo tee $PATH_RULE/noinet_rule
+fi' | sudo tee $PATH_RULE/noinet_rule
 sudo chmod +x $PATH_RULE/noinet_rule
 
-echo '\
-#!/bin/sh
-if [ $1 = "lo" ]; then
+echo '#!/bin/sh
+if [ "$1" = "lo" ]; then
   iptables -A OUTPUT -m owner --gid-owner nolan -j DROP
-fi
-' | sudo tee $PATH_RULE/nolan_rule
+fi' | sudo tee $PATH_RULE/nolan_rule
 sudo chmod +x $PATH_RULE/nolan_rule
 
 #EXEC
-echo '
-#!/bin/sh
-sg noinet "$*"
-' | sudo tee $PATH_EXEC/noinet
+echo '#!/bin/sh
+sg noinet "$*"' | sudo tee $PATH_EXEC/noinet
 sudo chmod +x $PATH_EXEC/noinet
 
-echo '
-#!/bin/sh
-sg nolan "$*"
-' | sudo tee $PATH_EXEC/nolan
+echo '#!/bin/sh
+sg nolan "$*"' | sudo tee $PATH_EXEC/nolan
 sudo chmod +x $PATH_EXEC/nolan
 
 #INIT
-sudo $PATH_RULE/noinet_rule
-sudo $PATH_RULE/nolan_rule
+sudo $PATH_RULE/noinet_rule lo
+sudo $PATH_RULE/nolan_rule lo
 
 #TEST
 ping -c 2 1.1.1.1
 noinet ping -c 2 1.1.1.1
-nolan ping -c 2 192.168.1.1
+nolan ping -c 2 127.0.0.1
